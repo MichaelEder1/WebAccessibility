5044a443c0ab21370147c8e136d750dc6933c22f
Michael Eder <65296298+MichaelEder1@users.noreply.github.com>
2020-05-15 20:22:32 +0200

    Project

---
+++ Projekt_Barrierefreiheit/template/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+	
+	$( "#skip_menu" ).focusin(function() {
+		$(this).removeClass('hide');
+	});
+	$( "#skip_menu" ).focusout(function() {
+		$(this).addClass('hide');
+	});
+});
